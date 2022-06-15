7b9b9fc3455d718422a8a23917010e22454734b8
JoaoAbaca <joao.abaca@gmail.com>
2022-06-15 17:12:28 -0300

    Practica DOM # / Lista Compra Final

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-const inputColor = document.querySelector("#inputColor");
-const boton = document.querySelector("#boton");
-const textoHexa = document.querySelector("#textoHexa");
-const cardColor = document.querySelector("#cardColor");
+const carrito = document.querySelector('#carrito');
+const template = document.querySelector('#template');
+const fragment = document.createDocumentFragment();
+const btnesBotones = document.querySelectorAll('.card .btn');
 
-console.log(inputColor.value);
+const carritoObjeto={};
 
-boton.addEventListener("click", () => {
-    console.log(inputColor.value);
-    textoHexa.textContent = inputColor.value;
-    cardColor.style.backgroundColor = inputColor.value;
-});
+const agregarAlCarrito = (e) => {
+    
+    const producto = {
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad: 1,
+    }; 
+    if (carritoObjeto.hasOwnProperty(producto.id)){
+        producto.cantidad = carritoObjeto[producto.id].cantidad +1;    
+    }    
+    carritoObjeto[producto.titulo]=producto;
+    pintarCarrito();
+};
+
+
+btnesBotones.forEach( (btn) => btn.addEventListener("click", agregarAlCarrito));
+
+const pintarCarrito = () => {
+
+    carrito.textContent= ""; 
+
+    Object.values(carritoObjeto).forEach((item) => {
+        const clone = template.content.firstElementChild.cloneNode(true);
+        clone.querySelector('.lead').textContent= item.titulo;
+        clone.querySelector('.rounded-pill').textContent= item.cantidad;
+        fragment.appendChild(clone);
+    })
+    carrito.appendChild(fragment);
+};
+
