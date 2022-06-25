85cd989181d40efd08a488e63b03c3b60ae22c78
JoaoAbaca <joao.abaca@gmail.com>
2022-06-25 19:04:36 -0300

    Practica Fetch V2

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-const cards = document.getElementById("card-dinamicas")
-const templateCard = document.getElementById("template-card").content
-const fragment = document.createDocumentFragment();
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
     fetchData();
 });
 
-const fetchData= async () => {
-    try{
+const cards = document.querySelector("#card-dinamica");
+const templateCard = document.querySelector("#template-card").content;
+
+const fetchData = async () => {
+    try {
         loadingData(true);
 
-        const res = await fetch("https://rickandmortyapi.com/api/character"); 
+        const res = await fetch("https://rickandmortyapi.com/api/character");
         const data = await res.json();
 
-
-    }   catch (error){
+        pintarDatos(data);
+    } catch (error) {
         console.log(error);
-    } finally{
+    } finally {
         loadingData(false);
     }
-
 };
 
-const pintarCard = (data) =>{
-    data.result.array.forEach(item => {
+const loadingData = (estado) => {
+    const loading = document.querySelector("#loading");
+    if (estado) {
+        loading.classList.remove("d-none");
+    } else {
+        loading.classList.add("d-none");
+    }
+};
+
+const pintarDatos = (data) => {
+    const fragment = document.createDocumentFragment();
+
+    cards.textContent = "";
+
+    data.results.forEach((item) => {
         const clone = templateCard.cloneNode(true);
-        clone.querySelector("h5").textContent=item.name;
+        clone.querySelector("h5").textContent = item.name;
         clone.querySelector("p").textContent = item.species;
-        clone.querySelector(".card-img-top").setAttribute("src", item.image); 
-         
+        clone.querySelector("img").setAttribute("src", item.image);
+
         fragment.appendChild(clone);
     });
     cards.appendChild(fragment);
-}   
-
-const loadingData = estado => {
-    const loading = document.querySelector("loading");
-    if(estado){
-    loading.classList.remove('d-none');
-    } else{
-        loading.classList.add("d-none");
-    }
-}
+};
