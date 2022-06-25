f467251a7c4820a7c7ae265c1a6d7305c722bac4
JoaoAbaca <joao.abaca@gmail.com>
2022-06-25 18:47:30 -0300

    Practica Fetch V1

--- app.js
+++ app.js
@@ -1,62 +1,43 @@
-const formulario=document.getElementById("formulario");
-
-const userName=document.getElementById("UserName");
-const userEmail=document.getElementById("UserEmail");
-
-const alertSuccess= document.getElementById("alertSuccess");
-const alertEmail= document.getElementById("alertEmail");
-const alertName= document.getElementById("alertName");
-
-const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
-
-const pintarMensajeExito = () =>{
-    alertSuccess.classList.remove("d-none");
-    alertSuccess.textContent="Mensaje enviado con exito";
-};
-const pintarMensajeErrores = (errores) =>{
-    errores.forEach(item => {
-        item.tipo.classList.remove("d-none");
-        item.tipo.textContent = item.msg;
-    });
-};
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    alertSuccess.classList.add("d-none");
-    
-    const errores= [];
-    //console.log(!userName.value.trim()); Devuelve true si solo tiene espacios
-
-    if(!regUserName.test(userName.value) || !userName.value.trim()){
-        userName.classList.add("is-invalid");
-        
-        errores.push({
-            tipo: alertName,
-            msg: "Formato no valido en el campo, solo letras",
-        });
-    } else {
-        userName.classList.remove("is-invalid");
-        userName.classList.add("is-valid");
-        alertName.classList.add("d-none");
-    };
-
-    if(!regUserEmail.test(userEmail.value) || !userName.value.trim()){
-        userEmail.classList.add("is-invalid");
-        
-        errores.push({
-            tipo: alertEmail,
-            msg: "Correo no valido",
-        });
-    } else {
-        userEmail.classList.remove("is-invalid");
-        userEmail.classList.add("is-valid");
-        alertEmail.classList.add("d-none");
-    };
-    if(errores.length != 0){
-        pintarMensajeErrores(errores);
-        return;
-    };
-    console.log("Formulario enviado con éxito");
-    pintarMensajeExito();
+const cards = document.getElementById("card-dinamicas")
+const templateCard = document.getElementById("template-card").content
+const fragment = document.createDocumentFragment();
+document.addEventListener("DOMContentLoaded",() => {
+    fetchData();
 });
+
+const fetchData= async () => {
+    try{
+        loadingData(true);
+
+        const res = await fetch("https://rickandmortyapi.com/api/character"); 
+        const data = await res.json();
+
+
+    }   catch (error){
+        console.log(error);
+    } finally{
+        loadingData(false);
+    }
+
+};
+
+const pintarCard = (data) =>{
+    data.result.array.forEach(item => {
+        const clone = templateCard.cloneNode(true);
+        clone.querySelector("h5").textContent=item.name;
+        clone.querySelector("p").textContent = item.species;
+        clone.querySelector(".card-img-top").setAttribute("src", item.image); 
+         
+        fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+}   
+
+const loadingData = estado => {
+    const loading = document.querySelector("loading");
+    if(estado){
+    loading.classList.remove('d-none');
+    } else{
+        loading.classList.add("d-none");
+    }
+}
